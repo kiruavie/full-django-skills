3786b135a68694db593a79e448cf17a673a60ee2
Magloire <debabel1hackamani@gmail.com>
2024-05-15 09:23:05 +0000

    urls

--- fulldjango/urls.py
+++ fulldjango/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from pages.views import home
+from pages.views import home, about, contact
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", home, name="home")
+    path("", home, name="home"),
+    path("about/", about, name="about"),
+    path("contact/", contact, name="contact"),
     
 ]
--- pages/views.py
+++ pages/views.py
@@ -5,3 +5,12 @@ from django.http import HttpResponse
 # Create your views here.
 def home(request):
     return HttpResponse("<h1>Home</h1>")
+
+
+def about(request):
+    return HttpResponse("About page")
+
+
+def contact(request):
+    print(request.user)
+    return HttpResponse("Contacts page")
